580cde08edc68b48a627027cf9217a29b78e236d
Robert Bunch <rdbunch@gmail.com>
2016-01-06 16:01:09 -0500

    Update login to include admin query

--- login.php
+++ login.php
@@ -1,3 +1,30 @@
+<?php
+
+	include 'inc/db_connect.php';
+
+	if(isset($_POST['email'])){
+		// print $_POST['email'];
+		// print $_POST['password'];
+
+		//USER SUBMITTED SOMETHING.
+		//NOW WHAT?
+		//Check to see if this user is in the DB!!
+		$query = "SELECT * FROM users 
+			WHERE email = '" . $_POST['email']."' 
+				AND password = '".$_POST['password']."' 
+				AND access_level = 1";
+
+		$result = mysql_query($query);
+		if(mysql_num_rows($result) == 1){
+			//We have a match!!
+
+		}else{
+			//we do not have a match. Goodbye.
+			header('Location: http://local-phpland.com/login.php?result=failure');
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,12 +40,17 @@
 </head>
 <body>
 
-	<form class="form-signin">
+<?php 
+	if($_GET['result'] == "failure"){
+		print "<h1>Your login information does not match any record in our system. Please retry or contact Stephen.";
+	}
+?>
+	<form class="form-signin" method="post" action="login.php">
 		<h2 class="form-signin-heading">Please sign in</h2>
 		<label for="inputEmail" class="sr-only">Email address</label>
-		<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+		<input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required="" autofocus="">
 		<label for="inputPassword" class="sr-only">Password</label>
-		<input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+		<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required="">
 		<div class="checkbox">
 			<label>
 			<input type="checkbox" value="remember-me"> Remember me
@@ -26,6 +58,7 @@
 		</div>
 		<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 		</form>
+}
 
 </body>
 </html>
